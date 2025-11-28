ddba859563a6386c6452a5c1b8afd51d1b09a882
Pierre Elie <pierrelielvl@gmail.com>
2025-11-28 14:41:13 +0100

    change_test

--- tests/unit/test_prepare_data.py
+++ tests/unit/test_prepare_data.py
@@ -2,7 +2,7 @@ from src.handle_data import *
 from src.prepare_data import *
 
 def test_vectorize_text_data():
-    file_path = "data/mock.jsonl"
+    file_path = "data/mock_prepare_data.jsonl"
     corpus = load_corpus_title(file_path)
     vectorizer, tab = vectorize_text_data(corpus)
     
