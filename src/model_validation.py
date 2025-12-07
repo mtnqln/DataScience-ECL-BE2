3a7abb0c5bafd48227c1134c01ca08dfee649534
Pierre Elie <pierrelielvl@gmail.com>
2025-12-07 23:37:34 +0100

    last_changes

--- src/model_validation.py
+++ src/model_validation.py
@@ -1,16 +1,21 @@
-from prepare_data import vectorize_data, vectorize_with_sentence_transformer
-from handle_data import load_corpus, load_queries, load_qrels
+from prepare_data import embeddings_creux, embeddings_dense, embedding_query_dense
+from handle_data import load_corpus, load_queries, load_qrels, load_sample_submission
 
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics import precision_score, recall_score, f1_score, roc_auc_score, precision_recall_curve
-from sentence_transformers import SentenceTransformer
 import numpy as np
-import os
+import pandas as pd
 
 
-def validate_model_creux(queries, corpus, valid):
-    matrix, vectorizer = vectorize_data(corpus)
+def validate_model(queries, corpus, valid, model_type='dense'):
+    if model_type == 'dense':
+        embeddings, dico, lda_model, embedding_model = embeddings_dense(corpus)
+    elif model_type == 'creux':
+        embeddings, model = embeddings_creux(corpus)
+    else:
+        raise ValueError("model_type not valid")
 
+    print("Embeddings loaded.")
     corpus_ids = list(corpus.keys())
     all_true_labels = []
     all_pred_labels = []
@@ -20,52 +25,13 @@ def validate_model_creux(queries, corpus, valid):
         # Création du vecteur de la requête
         query = queries[query_id]
         query_text = query['text']
-        query_vector = vectorizer.transform([query_text])
-
-        # Création des vecteurs des documents candidats
-        query_candidates_id = valid[query_id].keys()
-        candidate_index = [corpus_ids.index(candidate_id) for candidate_id in query_candidates_id]
-        query_candidate_vectors = matrix[candidate_index]
-
-        # Score de similarité entre la requête et les documents candidats
-        similarity_matrix = cosine_similarity(query_vector, query_candidate_vectors)
-        scores = similarity_matrix.flatten()
-
-        # On choisit les 5 meilleurs scores
-        scores_sorted = np.sort(scores)[::-1]
-        best_scores = scores_sorted[:5]
-
-        all_true_labels.extend([valid[query_id][candidate_id] for candidate_id in query_candidates_id])
-        all_pred_continuous_labels.extend(scores)
-        all_pred_labels.extend([1 if score in best_scores else 0 for score in scores])
-
-    # Calcul des métriques de performance de prédiction
-    precision = precision_score(np.array(all_true_labels), np.array(all_pred_labels), average='binary')
-    recall = recall_score(np.array(all_true_labels), np.array(all_pred_labels), average='binary')
-    f1 = f1_score(np.array(all_true_labels), np.array(all_pred_labels), average='binary')
-    auc_score = roc_auc_score(np.array(all_true_labels), np.array(all_pred_continuous_labels))
-
-    print(f"Precision: {precision}\nRecall: {recall}\nF1 Score: {f1}\nAUC Score: {auc_score}")
-
-
-def validate_model_dense(queries, corpus, valid):
-    if "embeddings.npy" not in os.listdir("data"):
-        embeddings, model = vectorize_with_sentence_transformer(corpus)
-        np.save("data/embeddings.npy", embeddings)
-
-    embeddings = np.load("data/embeddings.npy")
-    model = SentenceTransformer('all-MiniLM-L6-v2')
-
-    corpus_ids = list(corpus.keys())
-    all_true_labels = []
-    all_pred_labels = []
-    all_pred_continuous_labels = []
-
-    for query_id in valid.keys():
-        # Création du vecteur de la requête
-        query = queries[query_id]
-        query_text = query['text']
-        query_vector = model.encode([query_text])
+        print(f"Processing query ID: {query_text}")
+        
+        if model_type == 'dense':
+            # query_vector = model.encode([query_text])
+            query_vector = embedding_query_dense(query_text, dico, lda_model, embedding_model)
+        elif model_type == 'creux':
+            query_vector = model.transform([query_text])
 
         # Création des vecteurs des documents candidats
         query_candidates_id = valid[query_id].keys()
@@ -95,9 +61,59 @@ def validate_model_dense(queries, corpus, valid):
     print(f"Precision: {precision}\nRecall: {recall}\nF1 Score: {f1}\nAUC Score: {auc_score}")
 
 
+def sample_prediction(queries, corpus, valid, model_type='dense'):
+    if model_type == 'dense':
+        embeddings, dico, lda_model, embedding_model = embeddings_dense(corpus)
+    elif model_type == 'creux':
+        embeddings, model = embeddings_creux(corpus)
+    else:
+        raise ValueError("model_type not valid")
+
+    print(valid)
+
+    print("Embeddings loaded.")
+    corpus_ids = list(corpus.keys())
+    all_true_labels = []
+    all_pred_labels = []
+    all_pred_continuous_labels = []
+
+    for query_id in valid['query-id'].unique():
+        # Création du vecteur de la requête
+        query = queries[query_id]
+        query_text = query['text']
+        print(f"Processing query ID: {query_text}")
+        
+        if model_type == 'dense':
+            # query_vector = model.encode([query_text])
+            query_vector = embedding_query_dense(query_text, dico, lda_model, embedding_model)
+        elif model_type == 'creux':
+            query_vector = model.transform([query_text])
+
+        # Création des vecteurs des documents candidats
+        query_candidates_id = valid[valid['query-id'] == query_id]['corpus-id'].tolist()
+        candidate_index = [corpus_ids.index(candidate_id) for candidate_id in query_candidates_id]
+        query_candidate_vectors = embeddings[candidate_index]
+
+        # Score de similarité entre la requête et les documents candidats
+        similarity_matrix = cosine_similarity(query_vector, query_candidate_vectors)
+        scores = similarity_matrix.flatten()
+        
+        # On choisit les 5 meilleurs scores
+        scores_sorted = np.sort(scores)[::-1]
+        best_scores = scores_sorted[:5]
+
+        for i, candidate_id in enumerate(query_candidates_id): 
+            valid.loc[(valid['query-id'] == query_id) & (valid['corpus-id'] == candidate_id), 'score'] = 1 if scores[i] in best_scores else 0
+            # valid.loc[(valid['query-id'] == query_id) & (valid['corpus-id'] == candidate_id), 'score'] = scores[i]
+    
+    valid.to_csv('data/sample_submission_predicted.csv', index=False)
+
+
 if __name__ == "__main__":
     corpus = load_corpus("data/corpus.jsonl")
     queries = load_queries("data/queries.jsonl")
     qrels_valid = load_qrels("data/valid.tsv")
+    sample_prediction_data = pd.read_csv("data/sample_submission.csv")
 
-    validate_model_dense(queries, corpus, qrels_valid)
+    # validate_model(queries, corpus, qrels_valid, model_type='dense')
+    sample_prediction(queries, corpus, sample_prediction_data, model_type='dense')
