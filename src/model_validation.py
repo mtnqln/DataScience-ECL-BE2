4145d0c5addb07584531a4edace483cd28aac243
Edgar <edgar.ledoux@ecl22.ec-lyon.fr>
2025-12-07 22:51:20 +0100

    model LDA

--- src/model_validation.py
+++ src/model_validation.py
@@ -1,16 +1,20 @@
-from prepare_data import vectorize_data, vectorize_with_sentence_transformer
+from prepare_data import embeddings_creux, embeddings_dense, embedding_query_dense
 from handle_data import load_corpus, load_queries, load_qrels
 
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics import precision_score, recall_score, f1_score, roc_auc_score, precision_recall_curve
-from sentence_transformers import SentenceTransformer
 import numpy as np
-import os
 
 
-def validate_model_creux(queries, corpus, valid):
-    matrix, vectorizer = vectorize_data(corpus)
+def validate_model(queries, corpus, valid, model_type='dense'):
+    if model_type == 'dense':
+        embeddings, dico, lda_model, embedding_model = embeddings_dense(corpus)
+    elif model_type == 'creux':
+        embeddings, model = embeddings_creux(corpus)
+    else:
+        raise ValueError("model_type not valid")
 
+    print("Embeddings loaded.")
     corpus_ids = list(corpus.keys())
     all_true_labels = []
     all_pred_labels = []
@@ -20,52 +24,13 @@ def validate_model_creux(queries, corpus, valid):
         # Création du vecteur de la requête
         query = queries[query_id]
         query_text = query['text']
-        query_vector = vectorizer.transform([query_text])
-
-        # Création des vecteurs des documents candidats
-        query_candidates_id = valid[query_id].keys()
-        candidate_index = [corpus_ids.index(candidate_id) for candidate_id in query_candidates_id]
-        query_candidate_vectors = matrix[candidate_index]
-
-        # Score de similarité entre la requête et les documents candidats
-        similarity_matrix = cosine_similarity(query_vector, query_candidate_vectors)
-        scores = similarity_matrix.flatten()
-
-        # On choisit les 5 meilleurs scores
-        scores_sorted = np.sort(scores)[::-1]
-        best_scores = scores_sorted[:5]
-
-        all_true_labels.extend([valid[query_id][candidate_id] for candidate_id in query_candidates_id])
-        all_pred_continuous_labels.extend(scores)
-        all_pred_labels.extend([1 if score in best_scores else 0 for score in scores])
-
-    # Calcul des métriques de performance de prédiction
-    precision = precision_score(np.array(all_true_labels), np.array(all_pred_labels), average='binary')
-    recall = recall_score(np.array(all_true_labels), np.array(all_pred_labels), average='binary')
-    f1 = f1_score(np.array(all_true_labels), np.array(all_pred_labels), average='binary')
-    auc_score = roc_auc_score(np.array(all_true_labels), np.array(all_pred_continuous_labels))
-
-    print(f"Precision: {precision}\nRecall: {recall}\nF1 Score: {f1}\nAUC Score: {auc_score}")
-
-
-def validate_model_dense(queries, corpus, valid):
-    if "embeddings.npy" not in os.listdir("data"):
-        embeddings, model = vectorize_with_sentence_transformer(corpus)
-        np.save("data/embeddings.npy", embeddings)
-
-    embeddings = np.load("data/embeddings.npy")
-    model = SentenceTransformer('all-MiniLM-L6-v2')
-
-    corpus_ids = list(corpus.keys())
-    all_true_labels = []
-    all_pred_labels = []
-    all_pred_continuous_labels = []
-
-    for query_id in valid.keys():
-        # Création du vecteur de la requête
-        query = queries[query_id]
-        query_text = query['text']
-        query_vector = model.encode([query_text])
+        print(f"Processing query ID: {query_text}")
+        
+        if model_type == 'dense':
+            # query_vector = model.encode([query_text])
+            query_vector = embedding_query_dense(query_text, dico, lda_model, embedding_model)
+        elif model_type == 'creux':
+            query_vector = model.transform([query_text])
 
         # Création des vecteurs des documents candidats
         query_candidates_id = valid[query_id].keys()
@@ -100,4 +65,4 @@ if __name__ == "__main__":
     queries = load_queries("data/queries.jsonl")
     qrels_valid = load_qrels("data/valid.tsv")
 
-    validate_model_dense(queries, corpus, qrels_valid)
+    validate_model(queries, corpus, qrels_valid, model_type='dense')
--- src/prepare_data.py
+++ src/prepare_data.py
@@ -1,6 +1,11 @@
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
+from gensim.utils import simple_preprocess
+from gensim.corpora import Dictionary
+from gensim.models import LdaModel
+import os
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 from sentence_transformers import SentenceTransformer
@@ -16,24 +21,106 @@ def prepare_for_vectorizer(corpus):
         titles[i] = titles[i] + ' ' + text[i]
     return titles
 
-def vectorize_data(corpus):
+def sent_to_words(sentences):
+    for sentence in sentences:
+        yield(simple_preprocess(str(sentence), deacc=True))
+
+def remove_stopwords(texts):
+    stop_words_set = set(ENGLISH_STOP_WORDS)
+    data_words_nostops = [[word for word in doc if word not in stop_words_set] for doc in texts]
+    return data_words_nostops
+
+def get_lda_features(corpus_text):
+    """Calcule la distribution de sujets (features LDA) pour une liste de textes."""    
+    lda_features = []
+
+    corpus_text_processed = list(sent_to_words(corpus_text))
+    corpus_text_processed = remove_stopwords(corpus_text_processed)
+
+    if "ldamodel.gensim" in os.listdir("data") and "dico.dict" in os.listdir("data"):
+        print("Loading existing LDA model...")
+        ldamodel = LdaModel.load("data/ldamodel.gensim")
+        dico = Dictionary.load("data/dico.dict")
+
+    else:
+        dico = Dictionary(corpus_text_processed)
+        dico.filter_extremes(no_below=10)
+        bow_corpus = [dico.doc2bow(doc) for doc in corpus_text_processed]
+        # corpus = [dico.doc2bow(text) for text in data_words_nostops]
+
+        ldamodel = LdaModel(
+            bow_corpus, 
+            num_topics=20,
+            id2word=dico, 
+            passes=100)
+    
+        ldamodel.save("data/ldamodel.gensim")
+        dico.save("data/dico.dict")
+    
+    for text in corpus_text_processed:
+        bow = dico.doc2bow(text)
+        
+        doc_topics = ldamodel.get_document_topics(bow, minimum_probability=0)
+        
+        vector = np.zeros(ldamodel.num_topics)
+        for topic_id, prob in doc_topics:
+            vector[topic_id] = prob
+        
+        lda_features.append(vector)
+    return np.array(lda_features), ldamodel, dico
+
+
+def embeddings_creux(corpus):
     corpus_text = prepare_for_vectorizer(corpus)
-    # Supprime les mots trop fréquents (and, of, what...)
-    stop_words_list = list(ENGLISH_STOP_WORDS)
+    corpus_text_processed = list(sent_to_words(corpus_text))
+    corpus_text_processed = remove_stopwords(corpus_text_processed)
 
     # Pondération TF
-    # vectorizer = CountVectorizer(stop_words=stop_words_list)
+    # model = CountVectorizer()
     # Pondération TFxIDF
-    vectorizer = TfidfVectorizer(stop_words=stop_words_list)
+    model = TfidfVectorizer()
+    matrix = model.fit_transform(corpus_text_processed)
 
-    matrix = vectorizer.fit_transform(corpus_text)
-    return matrix, vectorizer
-
-def vectorize_with_sentence_transformer(corpus):
-    corpus_text = prepare_for_vectorizer(corpus)
-    model = SentenceTransformer('all-MiniLM-L6-v2')
-    matrix = model.encode(corpus_text)
     return matrix, model
+
+def embedding_query_dense(query_text, dico, ldamodel, embedding_model):
+    '''Calcule le vecteur d'une requête en utilisant le modèle dense avec features LDA.'''
+    query_vector_dense = embedding_model.encode([query_text])
+    query_tokens = list(simple_preprocess(query_text, deacc=True))
+    bow_query = dico.doc2bow(query_tokens)
+    
+    doc_topics = ldamodel.get_document_topics(bow_query, minimum_probability=0)
+    
+    query_vector_lda = np.zeros((1, ldamodel.num_topics))
+    for topic_id, prob in doc_topics:
+        query_vector_lda[0, topic_id] = prob
+        
+    query_vector = np.concatenate((query_vector_dense, query_vector_lda), axis=1)
+    return query_vector
+
+
+def embeddings_dense(corpus):
+    '''Calcule les embeddings d'un corpus en utilisant un modèle dense avec features LDA.'''
+    embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
+
+    if "embeddings_2.npy" not in os.listdir("data"):
+        corpus_text = prepare_for_vectorizer(corpus)
+        print("Loading dense embeddings...")
+        # embeddings = model.encode(corpus_text)
+        embeddings = np.load("data/embeddings.npy")
+
+        print("Calculating LDA features...")
+        lda_features, lda_model, dico = get_lda_features(corpus_text)
+        embeddings_2 = np.concatenate((embeddings, lda_features), axis=1)
+        print("Saving embeddings with LDA features...")
+
+        np.save("data/embeddings_2.npy", embeddings_2)
+    else:
+        corpus_text = prepare_for_vectorizer(corpus)
+        lda_features, lda_model, dico = get_lda_features(corpus_text)
+        embeddings_2 = np.load("data/embeddings_2.npy")
+
+    return embeddings_2, dico, lda_model, embedding_model
 
 
 def cosine_similarity_matrix(matrix):
@@ -44,13 +131,13 @@ if __name__=="__main__":
 
     corpus = load_corpus("data/corpus.jsonl")
     corpus_text = prepare_for_vectorizer(corpus)
-    matrix, vectorizer = vectorize_data(corpus_text)
+    matrix, model = embeddings_creux(corpus)
     print(matrix[0])
     # print(matrix[1])
     # print(matrix)
 
-    voc_1 = print_feats(matrix[0], vectorizer.get_feature_names_out(), top_n = 50)
-    voc_2 = print_feats(matrix[1], vectorizer.get_feature_names_out(), top_n = 50)
+    voc_1 = print_feats(matrix[0], model.get_feature_names_out(), top_n = 50)
+    voc_2 = print_feats(matrix[1], model.get_feature_names_out(), top_n = 50)
 
     # print(voc_1)
     # print(voc_2)
@@ -68,7 +155,7 @@ if __name__=="__main__":
     # Création du DataFrame de distribution (Top 30 mots)
     word_distribution_df = print_feats(
         total_counts_vector, 
-        vectorizer.get_feature_names_out(), 
+        model.get_feature_names_out(), 
         top_n=30
     )
 
